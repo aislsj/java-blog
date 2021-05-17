7401846a93578088e4efb92be4dcdb8a6ac41d0d
lsj_ais <1971754369@qqq.com>
2021-05-17 17:45:49 +0800

    1

--- src/main/java/com/ais/controller/admin/CategoryController.java
+++ src/main/java/com/ais/controller/admin/CategoryController.java
@@ -11,14 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
-
     @GetMapping("/category")
     public String index(@PageableDefault(size = 3,sort = {"id"},direction = Sort.Direction.DESC)
                     Pageable pageable, Model model){
--- src/main/java/com/ais/controller/admin/IndexController.java
+++ src/main/java/com/ais/controller/admin/IndexController.java
@@ -2,26 +2,20 @@ package com.ais.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class IndexController {
 
-
-
     @GetMapping
     public String index(){
        return  "/admin/index/index";
     }
-
     @RequestMapping("/homepage")
     public String homepage(){
         return "/admin/index/homepage";
     }
-
-
 
 
 }
